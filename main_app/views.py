2e56bfebe374c107493b33b0b0d3e70ed3808c0c
tylerbertmore <tylercsmith94@gmail.com>
2020-11-04 04:47:40 +0700

    String Usernames

--- main_app/views.py
+++ main_app/views.py
@@ -25,14 +25,8 @@ def signup(request):
         return render(request, 'registration/signup.html', context)
 
 # Profile routes
-<<<<<<< HEAD
-def profile_detail(request, username):
-    user = User.objects.get(username=username)
-    profile = Profile.objects.get(user=user)
-=======
 def profile_detail(request, user_id):
     profile = Profile.objects.get(user=user_id)
->>>>>>> submain
     return render(request, 'profiles/detail.html', {'profile': profile})
 
 def my_profile(request):
